dc609259342bb8c5d9ee548ae6832c38e1ce208d
Laerte Quinui <laertequinuio@gmail.com>
2024-09-24 11:15:14 -0300

    ✨feat(Board): Call useBall and usePaddles hooks to display in screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,48 @@
+import { useState } from 'react'
 import './App.css'
+import Ball from './components/Board/useBall'
+import usePaddles from './components/Board/usePaddles'
 import Canvas from './components/Canvas'
 
 function App() {
-  const draw = (context: CanvasRenderingContext2D, count: number) => {
+  const [leftPosition, setLeftPosition] = useState(50)
+  const [rightPosition, setRightPosition] = useState(50)
+  const [ballPosition, setBallPosition] = useState({ x: 300, y: 250 })
+
+  const { leftPaddle, rightPaddle } = usePaddles(
+    leftPosition,
+    rightPosition,
+    setLeftPosition,
+    setRightPosition,
+  )
+
+  const { ball } = Ball(
+    ballPosition,
+    setBallPosition,
+    leftPosition,
+    rightPosition,
+  )
+
+  /**
+   * A function that animates the canvas.
+   * @param context - The canvas rendering context.
+   */
+  const animate = (context: CanvasRenderingContext2D) => {
+    // Clear the canvas
+    requestAnimationFrame(() => animate(context))
     context.clearRect(0, 0, context.canvas.width, context.canvas.height)
-    context.fillStyle = 'black'
-    const delta = count % 100
-    context.fillRect(10 + delta, 0, context.canvas.width, context.canvas.height)
+
+    // Draw paddles and ball on the canvas
+    leftPaddle(context)
+    rightPaddle(context)
+    ball(context)
   }
 
   return (
-    <>
+    <div className="flex h-full flex-col">
       <h1>Pong Game</h1>
-      <Canvas draw={draw} height={180} width={90} />
-    </>
+      <Canvas draw={animate} height={500} width={600} />
+    </div>
   )
 }
 
