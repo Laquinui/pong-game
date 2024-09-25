a67d0a30cfeb9047d0b7bdb470eb67db1d2c9a30
Laerte Quinui <laertequinuio@gmail.com>
2024-09-25 18:09:15 -0300

    ✨feat(Icons): Add React, Tailwind, TypeScript, and Vite icons

---
+++ src/components/Icons/Vite.tsx
@@ -0,0 +1,29 @@
+function Vite(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M10 4.5L16 3L14 9.5L16 9L12 16V11L9 12L10 4.5Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15 6.5L22 5L12 22L2 5L9.741 6.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default Vite
