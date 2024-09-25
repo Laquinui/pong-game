4623b363833bf098c3e7cac21e933c9937c95f97
Laerte Quinui <laertequinuio@gmail.com>
2024-09-25 18:09:36 -0300

    feat(Menu): Update menu component with icons and settings

--- src/components/Navbar/Menu.tsx
+++ src/components/Navbar/Menu.tsx
@@ -1,10 +1,17 @@
 import {
+  ArrowDownIcon,
+  ArrowUpIcon,
   SpeakerWaveIcon,
   SpeakerXMarkIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { useEffect, useState } from 'react'
 import mainSongTheme from '../../assets/musics/pong_main_theme.wav'
+import { useRunningGame } from '../../context/RunningGameContext'
+import React from '../Icons/React'
+import Tailwind from '../Icons/Tailwind'
+import TypeScript from '../Icons/TypeScript'
+import Vite from '../Icons/Vite'
 
 function Menu({
   menuOpen,
@@ -13,19 +20,16 @@ function Menu({
   menuOpen: boolean
   setMenuOpen: (open: boolean) => void
 }) {
-  const [isPlaying, setIsPlaying] = useState(false)
+  const { gameRunning } = useRunningGame()
+
   const [volume, setVolume] = useState(0.5)
   const [music] = useState(new Audio(mainSongTheme))
   music.loop = true
 
-  const handlePlay = () => {
-    setIsPlaying((prevIsPlaying) => {
-      if (prevIsPlaying) music.pause()
-      else music.play()
-
-      return !prevIsPlaying
-    })
-  }
+  useEffect(() => {
+    if (gameRunning) music.play()
+    else music.pause()
+  }, [gameRunning, music])
 
   const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setVolume(Number(event.target.value))
@@ -37,11 +41,11 @@ function Menu({
 
   return (
     <div className={`menu ${menuOpen ? 'show' : 'hide'}`}>
-      <div className="relative mx-auto flex w-[600px] flex-col gap-8 py-8 text-start">
+      <div className="relative mx-auto flex w-[600px] flex-col gap-10 py-8 text-start">
         <button
           type="button"
           aria-label="Close settings"
-          className="btn mb-8 ms-auto"
+          className="btn mb-6 ms-auto"
           onClick={() => setMenuOpen(false)}
         >
           <XMarkIcon className="size-6" />
@@ -49,34 +53,90 @@ function Menu({
 
         <h2>Settings</h2>
 
-        <ColItem title="Music">
+        <ColItem title="Music volume">
           <audio src={mainSongTheme} />
-          <div
-            className="flex w-fit cursor-pointer items-center gap-2 rounded-full px-4 py-2 duration-300 hover:bg-[#ffffff20]"
-            onClick={handlePlay}
-          >
-            {isPlaying ? (
-              <>
+          <div className="flex gap-4">
+            <div className="flex w-full flex-col gap-2">
+              <input
+                type="range"
+                min="0"
+                max="1"
+                step="0.05"
+                value={volume}
+                onChange={handleVolumeChange}
+                className="w-full"
+              />
+              <div className="flex w-full justify-between">
                 <SpeakerXMarkIcon className="size-6" />
-                <span>Mute music</span>
-              </>
-            ) : (
-              <>
                 <SpeakerWaveIcon className="size-6" />
-                <span>Pay music</span>
-              </>
-            )}
+              </div>
+            </div>
+            <span className="block text-start">
+              {(volume * 100).toFixed(0)}%
+            </span>
           </div>
-          <div className="flex gap-2">
-            <input
-              type="range"
-              min="0"
-              max="1"
-              step="0.05"
-              value={volume}
-              onChange={handleVolumeChange}
-            />
-            <span>{(volume * 100).toFixed(0)}%</span>
+        </ColItem>
+
+        <ColItem title="Controls">
+          <div className="flex h-full items-center gap-8">
+            <span>Player 1</span>
+            <span className="h-8 w-[2px] bg-[#ffffff20]"> </span>
+            <span>W</span>
+            <span>S</span>
+          </div>
+
+          <div className="flex h-full items-center gap-8">
+            <span>Player 2</span>
+            <span className="h-8 w-[2px] bg-[#ffffff20]"> </span>
+            <ArrowUpIcon className="size-4" />
+            <ArrowDownIcon className="size-4" />
+          </div>
+        </ColItem>
+
+        <ColItem title="Credits">
+          <div className="flex flex-col gap-1">
+            <span className="text-[#ffffff60]">About</span>
+            <span>A remake of the classic ATARI Pong game</span>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <span className="text-[#ffffff60]">Made by</span>
+            <span>Laerte Quinui</span>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <span className="text-[#ffffff60]">Tecnologies</span>
+            <div className="flex gap-4">
+              <BtnLink link="https://react.dev/" label="Open React site">
+                <React />
+              </BtnLink>
+
+              <BtnLink
+                link="https://www.typescriptlang.org/"
+                label="Open TypeScript site"
+              >
+                <TypeScript />
+              </BtnLink>
+
+              <BtnLink
+                link="https://tailwindcss.com/"
+                label="Open Tailwindcss site"
+              >
+                <Tailwind />
+              </BtnLink>
+
+              <BtnLink link="https://vitejs.dev/" label="Open Vite site">
+                <Vite />
+              </BtnLink>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <span className="text-[#ffffff60]">Music by</span>
+            <span>Laerte Quinui</span>
+            <span className="text-xs font-thin opacity-80">
+              © 2024 Laerte Quinui all rights reserved
+            </span>
           </div>
         </ColItem>
       </div>
@@ -93,9 +153,32 @@ const ColItem = ({
 }) => {
   return (
     <div className="flex w-1/2 flex-col justify-start gap-4 text-start">
-      <h3>{title}</h3>
+      <h3 className="mb-2">{title}</h3>
       {children}
     </div>
+  )
+}
+
+const BtnLink = ({
+  link,
+  label,
+  children,
+}: {
+  link: string
+  label: string
+  children: JSX.Element
+}) => {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noreferrer"
+      type="button"
+      className="btn"
+      aria-label={label}
+    >
+      {children}
+    </a>
   )
 }
 
